295c9fc8f1da07c94c382dfec562660034527a60
askrivenko <as.krivenko@stalcom.com>
2019-11-04 00:02:10 +0300

    htts forwarding off

--- Startup.cs
+++ Startup.cs
@@ -54,10 +54,10 @@ namespace Iot.WebApi
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
-			app.UseForwardedHeaders(new ForwardedHeadersOptions
-			{
-				ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-			});
+			//app.UseForwardedHeaders(new ForwardedHeadersOptions
+			//{
+			//	ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+			//});
 
 			if (env.IsDevelopment())
 			{
